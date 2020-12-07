497488e35930a28affa8a8da27e14b9d47932a6d
tphan025 <phantrungthanh25@gmail.com>
2020-12-07 18:09:48 +0100

    fixed

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
 function calcul_date() {
-    var date = 1606566720;
-    var time_stamp = Math.floor(Date.now() / 1000);
-    // console.log(time_stamp);
+    let date = 1606588320;
+    let time_stamp = Math.floor(Date.now() / 1000);
+    // console.log("-------");
     // console.log((time_stamp - date));
-    var date_since = Math.floor((time_stamp - date) / 86400);
+    let date_since = Math.floor((time_stamp - date) / 86400);
     // console.log(date_since);
-    var date =  new Date((time_stamp-date) * 1000).toISOString().substr(11, 8)
-    // console.log(date);
-    var h = date.substr(0,2);
-    var m = date.substr(3,2);
-    var s = date.substr(6,2);
-    var string = "Số ngày đợi thiệp mời cưới của bạn Mai: " + date_since + " ngày " + (h - date_since * 24) + " giờ " + m + " phút " + s + " giây";
-    // console.log(string);
+    let dt =  ((time_stamp - date) - (date_since * 86400)) // NB seconds left
+    // console.log((time_stamp - date));
+    // console.log(date_since * 86400);
+    // console.log(dt);
+    let h = Math.floor(dt / 3600);
+    let m = Math.floor((dt - h*3600)/60 );
+    let s = ((dt-h*3600) - m*60)
+    let string = "Số ngày đợi thiệp mời cưới của bạn Mai: " + date_since + " ngày " + (h) + " giờ " + m + " phút " + s + " giây";
     document.getElementById("display").innerHTML = string;
-}   
+}
 
 setInterval(calcul_date, 1000);
+
+function shiba() {
+    // let count = Math.floor(Math.random()*100)
+    let url = "http://shibe.online/api/shibes?count=1&urls=true&httpsUrls=false";
+
+    const Http = new XMLHttpRequest();
+    Http.open("GET", url);
+    Http.send();
+
+    Http.onreadystatechange = (e) => {
+        console.log(Http.responseText)
+    }
+}
